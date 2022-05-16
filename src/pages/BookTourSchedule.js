7834e7b4240d622195562c15a4fb8d5723b70bb8
ThienHN <thienviva7761311@gmail.com>
2022-05-16 23:38:15 +0700

    [ThienHN] Add Book Tour Of Schedule Tour Page

---
+++ src/pages/BookTourSchedule.js
@@ -0,0 +1,279 @@
+import { filter } from 'lodash';
+import { Icon } from '@iconify/react';
+import { sentenceCase } from 'change-case';
+import plusFill from '@iconify/icons-eva/plus-fill';
+import { Link as RouterLink } from 'react-router-dom';
+import { useState ,useEffect} from 'react';
+import callApi from 'src/api/apiService';
+import { useParams, useNavigate } from "react-router-dom";
+// material
+import {
+  Card,
+  Table,
+  Stack,
+  Avatar,
+  Button,
+  Checkbox,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+  TablePagination
+} from '@mui/material';
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+// components
+import Page from '../components/Page';
+import Label from '../components/Label';
+import Scrollbar from '../components/Scrollbar';
+import SearchNotFound from '../components/SearchNotFound';
+import { UserListHead, UserListToolbar, UserMoreMenu } from '../components/_dashboard/user';
+import BookTourMenu from '../components/_dashboard/user/BookTourMenu.js';
+//
+import USERLIST from '../_mocks_/user';
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'nameTour', label: 'Tên Tour', alignRight: false },
+  { id: 'email', label: 'Email', alignRight: false },
+  { id: 'nameUser', label: 'Khách hàng', alignRight: false },
+  { id: 'payment', label: 'Giá(VNĐ)', alignRight: false , align: 'right', format: (value) => value.toLocaleString('en-US')},
+  { id: 'startDate', label: 'Ngày đi', alignRight: false },
+  { id: 'endDate', label: 'Ngày về', alignRight: false },
+  { id: 'status', label: 'Trạng thái', alignRight: false },
+  { id: '' }
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
+
+
+
+export default function BookTourSchedule() {
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState('asc');
+  const [selected, setSelected] = useState([]);
+  const [orderBy, setOrderBy] = useState('name');
+  const [filterName, setFilterName] = useState('');
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  //lấy danh sách user
+  const [allBookTourSchedule, setBookTourSchedule] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    callApi(
+      `scheduletour/getBookTourOfScheduleTour?id=${id}`,
+      "GET"
+    ).then((res) => {
+      console.log(res.data.data)
+      setBookTourSchedule(res.data.data);
+    });
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = allBookTourSchedule.map((n) => n._id);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - allBookTourSchedule.length) : 0;
+
+  const filteredBookTourSchedule = applySortFilter(allBookTourSchedule, getComparator(order, orderBy), filterName);
+
+  const isUserNotFound = filteredBookTourSchedule.length === 0;
+
+
+  return (
+    <Page title="Các chỗ đã đặt của lịch trình | TravelApp">
+      <Container>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h4" gutterBottom>
+            Tour đã đặt (Số lượng {allBookTourSchedule.length})
+          </Typography>
+          
+        </Stack>
+
+        <Card>
+          <UserListToolbar
+            numSelected={selected.length}
+            filterName={filterName}
+            onFilterName={handleFilterByName}
+          />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <UserListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={allBookTourSchedule.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleRequestSort}
+                  onSelectAllClick={handleSelectAllClick}
+                />
+                <TableBody>
+                  {filteredBookTourSchedule
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      const { _id, finalpayment, status, startDate, endDate, idUser, idTour } = row;
+                      const isItemSelected = selected.indexOf(_id) !== -1;
+
+                      return (
+                        <TableRow
+                          hover
+                          key={_id}
+                          tabIndex={-1}
+                          role="checkbox"
+                          selected={isItemSelected}
+                          aria-checked={isItemSelected}
+                        >
+                          <TableCell padding="checkbox">
+                            <Checkbox
+                              checked={isItemSelected}
+                              onChange={(event) => handleClick(event, _id)}
+                            />
+                          </TableCell>
+                          <TableCell component="th" scope="row" padding="none" sx={{ maxWidth: 350 }}>
+                            <Stack direction="row" alignItems="center" spacing={2}>
+                              <Avatar alt={_id} src={row.tour.imagesTour[0]} />                            
+                              <Typography variant="subtitle2" noWrap>
+                                {row.tour.name}                              
+                              </Typography>
+                            </Stack>
+                          </TableCell>
+                          <TableCell align="left" sx={{ maxWidth: 180 }}>{row.user.email}</TableCell>
+                          <TableCell align="left">{row.user.name}</TableCell>
+                          <TableCell align="left" >{finalpayment.toLocaleString('en-US')}  VNĐ</TableCell>
+                          <TableCell align="left" >{new Date(startDate).toISOString('vi-VN').slice(0, 10)}</TableCell>
+                          <TableCell align="left">{new Date(endDate).toISOString('vi-VN').slice(0, 10)}</TableCell>
+                          <TableCell align="left">
+                            <Label
+                              variant="ghost"
+                              color={(status == 1 && 'success') || (status == 2 && 'error') || 'info'}
+                            >
+                              {(status == 1 && 'Đã Đặt') || (status == 2 && 'Đã Hủy') || 'Đang chờ'}
+                            </Label>
+                          </TableCell>
+
+                          <TableCell align="right">
+                            <BookTourMenu id={_id} status={status}/>
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                {isUserNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={allBookTourSchedule.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>   
+
+    </Page>
+  );
+}
